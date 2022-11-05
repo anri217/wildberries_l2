4c492f95d73430b27e559a3614528aef9c2dfacb
andreyryazanov <apple556@yandex.ru>
2022-11-05 20:17:22 +0400

    Add builder, visitor and command patterns

--- pattern/02_builder.go
+++ pattern/02_builder.go
@@ -1,7 +1,167 @@
 package pattern
 
+import "fmt"
+
 /*
 	Реализовать паттерн «строитель».
 Объяснить применимость паттерна, его плюсы и минусы, а также реальные примеры использования данного примера на практике.
 	https://en.wikipedia.org/wiki/Builder_pattern
 */
+
+/*
+	Строитель — это порождающий паттерн проектирования, который позволяет создавать сложные объекты пошагово.
+	Строитель даёт возможность использовать один и тот же код строительства для получения разных представлений объектов.
+
+
+	ПРИМЕНИМОСТЬ
+
+	Когда вы хотите избавиться от «телескопического конструктора».
+	Допустим, у вас есть один конструктор с десятью опциональными параметрами.
+	Его неудобно вызывать, поэтому вы создали ещё десять конструкторов с меньшим количеством параметров.
+	Всё, что они делают — это переадресуют вызов к базовому конструктору, подавая какие-то значения по умолчанию в параметры,
+	которые пропущены в них самих.
+
+	Когда вам нужно собирать сложные составные объекты, например, деревья Компоновщика.
+	Строитель конструирует объекты пошагово, а не за один проход. Более того, шаги строительства можно выполнять рекурсивно.
+	А без этого не построить древовидную структуру, вроде Компоновщика. Заметьте, что Строитель не позволяет посторонним
+	объектам иметь доступ к конструируемому объекту, пока тот не будет полностью готов. Это предохраняет клиентский код от
+	получения незаконченных «битых» объектов.
+
+
+	ПРЕИМУЩЕСТВА
+
+	Позволяет создавать продукты пошагово.
+ 	Позволяет использовать один и тот же код для создания различных продуктов.
+ 	Изолирует сложный код сборки продукта от его основной бизнес-логики.
+
+
+	НЕДОСТАТКИ
+
+	Усложняет код программы из-за введения дополнительных классов.
+	Клиент будет привязан к конкретным классам строителей, так как в интерфейсе директора может не быть метода получения результата.
+*/
+
+type House struct {
+	windowType string
+	doorType   string
+	floor      int
+}
+
+type IBuilder interface {
+	setWindowType()
+	setDoorType()
+	setNumFloor()
+	getHouse() House
+}
+
+func getBuilder(builderType string) IBuilder {
+	if builderType == "normal" {
+		return newNormalBuilder()
+	}
+
+	if builderType == "igloo" {
+		return newIglooBuilder()
+	}
+	return nil
+}
+
+type IglooBuilder struct {
+	windowType string
+	doorType   string
+	floor      int
+}
+
+func newIglooBuilder() *IglooBuilder {
+	return &IglooBuilder{}
+}
+
+func (b *IglooBuilder) setWindowType() {
+	b.windowType = "Snow Window"
+}
+
+func (b *IglooBuilder) setDoorType() {
+	b.doorType = "Snow Door"
+}
+
+func (b *IglooBuilder) setNumFloor() {
+	b.floor = 1
+}
+
+func (b *IglooBuilder) getHouse() House {
+	return House{
+		doorType:   b.doorType,
+		windowType: b.windowType,
+		floor:      b.floor,
+	}
+}
+
+type Director struct {
+	builder IBuilder
+}
+
+func newDirector(b IBuilder) *Director {
+	return &Director{
+		builder: b,
+	}
+}
+
+func (d *Director) setBuilder(b IBuilder) {
+	d.builder = b
+}
+
+func (d *Director) buildHouse() House {
+	d.builder.setDoorType()
+	d.builder.setWindowType()
+	d.builder.setNumFloor()
+	return d.builder.getHouse()
+}
+
+type NormalBuilder struct {
+	windowType string
+	doorType   string
+	floor      int
+}
+
+func newNormalBuilder() *NormalBuilder {
+	return &NormalBuilder{}
+}
+
+func (b *NormalBuilder) setWindowType() {
+	b.windowType = "Wooden Window"
+}
+
+func (b *NormalBuilder) setDoorType() {
+	b.doorType = "Wooden Door"
+}
+
+func (b *NormalBuilder) setNumFloor() {
+	b.floor = 2
+}
+
+func (b *NormalBuilder) getHouse() House {
+	return House{
+		doorType:   b.doorType,
+		windowType: b.windowType,
+		floor:      b.floor,
+	}
+}
+
+func main() {
+	normalBuilder := getBuilder("normal")
+	iglooBuilder := getBuilder("igloo")
+
+	director := newDirector(normalBuilder)
+	normalHouse := director.buildHouse()
+
+	fmt.Printf("Normal House Door Type: %s\n", normalHouse.doorType)
+	fmt.Printf("Normal House Window Type: %s\n", normalHouse.windowType)
+	fmt.Printf("Normal House Num Floor: %d\n", normalHouse.floor)
+
+	director.setBuilder(iglooBuilder)
+	iglooHouse := director.buildHouse()
+
+	fmt.Printf("\nIgloo House Door Type: %s\n", iglooHouse.doorType)
+	fmt.Printf("Igloo House Window Type: %s\n", iglooHouse.windowType)
+	fmt.Printf("Igloo House Num Floor: %d\n", iglooHouse.floor)
+
+}
--- pattern/03_visitor.go
+++ pattern/03_visitor.go
@@ -1,7 +1,140 @@
 package pattern
 
+import "fmt"
+
 /*
 	Реализовать паттерн «посетитель».
 Объяснить применимость паттерна, его плюсы и минусы, а также реальные примеры использования данного примера на практике.
 	https://en.wikipedia.org/wiki/Visitor_pattern
 */
+
+/*
+	Посетитель — это поведенческий паттерн проектирования, который позволяет добавлять в программу новые операции,
+	не изменяя классы объектов, над которыми эти операции могут выполняться.
+
+
+	ПРИМЕНИМОСТЬ
+
+	Когда вам нужно выполнить какую-то операцию над всеми элементами сложной структуры объектов, например, деревом.
+	Посетитель позволяет применять одну и ту же операцию к объектам различных классов.
+
+	Когда над объектами сложной структуры объектов надо выполнять некоторые не связанные между собой операции,
+	но вы не хотите «засорять» классы такими операциями.
+	Посетитель позволяет извлечь родственные операции из классов, составляющих структуру объектов, поместив их в один класс-посетитель.
+	Если структура объектов является общей для нескольких приложений, то паттерн позволит
+	в каждое приложение включить только нужные операции.
+
+
+	ПРЕИМУЩЕСТВА
+
+	Упрощает добавление операций, работающих со сложными структурами объектов.
+	Объединяет родственные операции в одном классе.
+	Посетитель может накапливать состояние при обходе структуры элементов.
+
+
+	НЕДОСТАТКИ
+
+	Паттерн не оправдан, если иерархия элементов часто меняется.
+	Может привести к нарушению инкапсуляции элементов.
+*/
+
+type AreaCalculator struct {
+	area int
+}
+
+func (a *AreaCalculator) visitForSquare(s *Square) {
+	// Calculate area for square.
+	// Then assign in to the area instance variable.
+	fmt.Println("Calculating area for square")
+}
+
+func (a *AreaCalculator) visitForCircle(s *Circle) {
+	fmt.Println("Calculating area for circle")
+}
+func (a *AreaCalculator) visitForrectangle(s *Rectangle) {
+	fmt.Println("Calculating area for rectangle")
+}
+
+type Circle struct {
+	radius int
+}
+
+func (c *Circle) accept(v Visitor) {
+	v.visitForCircle(c)
+}
+
+func (c *Circle) getType() string {
+	return "Circle"
+}
+
+type MiddleCoordinates struct {
+	x int
+	y int
+}
+
+func (a *MiddleCoordinates) visitForSquare(s *Square) {
+	// Calculate middle point coordinates for square.
+	// Then assign in to the x and y instance variable.
+	fmt.Println("Calculating middle point coordinates for square")
+}
+
+func (a *MiddleCoordinates) visitForCircle(c *Circle) {
+	fmt.Println("Calculating middle point coordinates for circle")
+}
+func (a *MiddleCoordinates) visitForrectangle(t *Rectangle) {
+	fmt.Println("Calculating middle point coordinates for rectangle")
+}
+
+type Rectangle struct {
+	l int
+	b int
+}
+
+func (t *Rectangle) accept(v Visitor) {
+	v.visitForrectangle(t)
+}
+
+func (t *Rectangle) getType() string {
+	return "rectangle"
+}
+
+type Shape interface {
+	getType() string
+	accept(Visitor)
+}
+
+type Square struct {
+	side int
+}
+
+func (s *Square) accept(v Visitor) {
+	v.visitForSquare(s)
+}
+
+func (s *Square) getType() string {
+	return "Square"
+}
+
+type Visitor interface {
+	visitForSquare(*Square)
+	visitForCircle(*Circle)
+	visitForrectangle(*Rectangle)
+}
+
+func main() {
+	square := &Square{side: 2}
+	circle := &Circle{radius: 3}
+	rectangle := &Rectangle{l: 2, b: 3}
+
+	areaCalculator := &AreaCalculator{}
+
+	square.accept(areaCalculator)
+	circle.accept(areaCalculator)
+	rectangle.accept(areaCalculator)
+
+	fmt.Println()
+	middleCoordinates := &MiddleCoordinates{}
+	square.accept(middleCoordinates)
+	circle.accept(middleCoordinates)
+	rectangle.accept(middleCoordinates)
+}
