dcb40ab87a215f44453fe1b2349b982f7371d036
andreyryazanov <apple556@yandex.ru>
2022-11-06 21:52:53 +0400

    Add chain of resp, factory, strat and state

--- pattern/06_factory_method.go
+++ pattern/06_factory_method.go
@@ -1,7 +1,115 @@
 package pattern
 
+import "fmt"
+
 /*
 	Реализовать паттерн «фабричный метод».
 Объяснить применимость паттерна, его плюсы и минусы, а также реальные примеры использования данного примера на практике.
 	https://en.wikipedia.org/wiki/Factory_method_pattern
 */
+
+/*
+	Фабричный метод — это порождающий паттерн проектирования, который определяет общий интерфейс для создания объектов в суперклассе,
+	позволяя подклассам изменять тип создаваемых объектов.
+
+
+	ПРИМЕНИМОСТЬ
+
+	Когда заранее неизвестны типы и зависимости объектов, с которыми должен работать ваш код.
+	Фабричный метод отделяет код производства продуктов от остального кода, который эти продукты использует.
+	Благодаря этому, код производства можно расширять, не трогая основной. Так, чтобы добавить поддержку нового продукта,
+	вам нужно создать новый подкласс и определить в нём фабричный метод, возвращая оттуда экземпляр нового продукта.
+
+
+	ПРЕИМУЩЕСТВА
+
+	Избавляет класс от привязки к конкретным классам продуктов.
+	Выделяет код производства продуктов в одно место, упрощая поддержку кода.
+	Упрощает добавление новых продуктов в программу.
+	Реализует принцип открытости/закрытости.
+
+
+	НЕДОСТАТКИ
+
+	Может привести к созданию больших параллельных иерархий классов, так как для каждого класса продукта надо создать
+	свой подкласс создателя.
+*/
+
+type Ak47 struct {
+	Gun
+}
+
+func newAk47() IGun {
+	return &Ak47{
+		Gun: Gun{
+			name:  "AK47 gun",
+			power: 4,
+		},
+	}
+}
+
+type Gun struct {
+	name  string
+	power int
+}
+
+func (g *Gun) setName(name string) {
+	g.name = name
+}
+
+func (g *Gun) getName() string {
+	return g.name
+}
+
+func (g *Gun) setPower(power int) {
+	g.power = power
+}
+
+func (g *Gun) getPower() int {
+	return g.power
+}
+
+func getGun(gunType string) (IGun, error) {
+	if gunType == "ak47" {
+		return newAk47(), nil
+	}
+	if gunType == "musket" {
+		return newMusket(), nil
+	}
+	return nil, fmt.Errorf("Wrong gun type passed")
+}
+
+type IGun interface {
+	setName(name string)
+	setPower(power int)
+	getName() string
+	getPower() int
+}
+
+type musket struct {
+	Gun
+}
+
+func newMusket() IGun {
+	return &musket{
+		Gun: Gun{
+			name:  "Musket gun",
+			power: 1,
+		},
+	}
+}
+
+func main() {
+	ak47, _ := getGun("ak47")
+	musket, _ := getGun("musket")
+
+	printDetails(ak47)
+	printDetails(musket)
+}
+
+func printDetails(g IGun) {
+	fmt.Printf("Gun: %s", g.getName())
+	fmt.Println()
+	fmt.Printf("Power: %d", g.getPower())
+	fmt.Println()
+}
--- pattern/07_strategy.go
+++ pattern/07_strategy.go
@@ -1,7 +1,123 @@
 package pattern
 
+import "fmt"
+
 /*
 	Реализовать паттерн «стратегия».
 Объяснить применимость паттерна, его плюсы и минусы, а также реальные примеры использования данного примера на практике.
 	https://en.wikipedia.org/wiki/Strategy_pattern
 */
+
+/*
+	Стратегия — это поведенческий паттерн проектирования, который определяет семейство схожих алгоритмов и помещает каждый из них в
+	собственный класс, после чего алгоритмы можно взаимозаменять прямо во время исполнения программы.
+
+
+	ПРИМЕНИМОСТЬ
+
+	Когда вам нужно использовать разные вариации какого-то алгоритма внутри одного объекта.
+	Стратегия позволяет варьировать поведение объекта во время выполнения программы, подставляя в него различные
+	объекты-поведения (например, отличающиеся балансом скорости и потребления ресурсов).
+
+	Когда у вас есть множество похожих классов, отличающихся только некоторым поведением.
+	Стратегия позволяет вынести отличающееся поведение в отдельную иерархию классов, а затем свести первоначальные классы к одному,
+	сделав поведение этого класса настраиваемым.
+
+
+	ПРЕИМУЩЕСТВА
+
+	Горячая замена алгоритмов на лету.
+	Изолирует код и данные алгоритмов от остальных классов.
+	Уход от наследования к делегированию.
+	Реализует принцип открытости/закрытости.
+
+
+	НЕДОСТАТКИ
+
+	Усложняет программу за счёт дополнительных классов.
+	Клиент должен знать, в чём состоит разница между стратегиями, чтобы выбрать подходящую.
+*/
+
+type Cache struct {
+	storage      map[string]string
+	evictionAlgo EvictionAlgo
+	capacity     int
+	maxCapacity  int
+}
+
+func initCache(e EvictionAlgo) *Cache {
+	storage := make(map[string]string)
+	return &Cache{
+		storage:      storage,
+		evictionAlgo: e,
+		capacity:     0,
+		maxCapacity:  2,
+	}
+}
+
+func (c *Cache) setEvictionAlgo(e EvictionAlgo) {
+	c.evictionAlgo = e
+}
+
+func (c *Cache) add(key, value string) {
+	if c.capacity == c.maxCapacity {
+		c.evict()
+	}
+	c.capacity++
+	c.storage[key] = value
+}
+
+func (c *Cache) get(key string) {
+	delete(c.storage, key)
+}
+
+func (c *Cache) evict() {
+	c.evictionAlgo.evict(c)
+	c.capacity--
+}
+
+type EvictionAlgo interface {
+	evict(c *Cache)
+}
+
+type Fifo struct {
+}
+
+func (l *Fifo) evict(c *Cache) {
+	fmt.Println("Evicting by fifo strtegy")
+}
+
+type Lfu struct {
+}
+
+func (l *Lfu) evict(c *Cache) {
+	fmt.Println("Evicting by lfu strtegy")
+}
+
+type Lru struct {
+}
+
+func (l *Lru) evict(c *Cache) {
+	fmt.Println("Evicting by lru strtegy")
+}
+
+func main() {
+	lfu := &Lfu{}
+	cache := initCache(lfu)
+
+	cache.add("a", "1")
+	cache.add("b", "2")
+
+	cache.add("c", "3")
+
+	lru := &Lru{}
+	cache.setEvictionAlgo(lru)
+
+	cache.add("d", "4")
+
+	fifo := &Fifo{}
+	cache.setEvictionAlgo(fifo)
+
+	cache.add("e", "5")
+
+}
