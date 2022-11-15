314440967c6f1dcd6abde239bba51b71074b448e
andreyryazanov <apple556@yandex.ru>
2022-11-15 23:27:38 +0400

    Add dev02 solution

--- develop/dev02/task_test.go
+++ develop/dev02/task_test.go
@@ -1,1 +1,23 @@
 package main
+
+import "testing"
+
+func TestUnpackString(t *testing.T) {
+	answerStr := "aaaabccddddde"
+	str, err := UnpackStr("a4bc2d5e")
+	if err != nil {
+		t.Fatalf("Can't unpack your string: %v", err)
+	} else if str != answerStr {
+		t.Fatalf("The function is not working: %v", err)
+	}
+}
+
+func TestUnpackEscapeString(t *testing.T) {
+	answerStr := "qwe45"
+	str, err := UnpackStr(`qwe\4\5`)
+	if err != nil {
+		t.Fatalf("Can't unpack your string: %v", err)
+	} else if str != answerStr {
+		t.Fatalf("The function is not working: %v", err)
+	}
+}
