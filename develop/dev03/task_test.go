46f6565123d22d88cc851504cb21aa7fdc47039d
andreyryazanov <apple556@yandex.ru>
2022-11-28 15:26:14 +0400

    03-04

--- develop/dev03/task_test.go
+++ develop/dev03/task_test.go
@@ -1,1 +1,50 @@
 package main
+
+func TestQuickSortDefault(t *testing.T) {
+	flags := &flagsCmd{}
+	assert.Equal(t, quickSort([]string{"ba bc ce", "ab jk kk", "dd"}, *flags), []string{"ab jk kk", "ba bc ce", "dd"})
+}
+
+func TestQuickSortColumn(t *testing.T) {
+	flags := &flagsCmd{k: 2}
+	assert.Equal(t, quickSort([]string{"ba bc ce", "ab jk kk", "dd er", "aa cc kk"}, *flags), []string{"ba bc ce", "aa cc kk", "dd er", "ab jk kk"})
+}
+
+func TestQuickSortNumber(t *testing.T) {
+	flags := &flagsCmd{n: true}
+	assert.Equal(t, quickSort([]string{"4 5 6", "1 2 3", "2 2 3"}, *flags), []string{"1 2 3", "2 2 3", "4 5 6"})
+}
+
+func TestQuickSortNumberColumn(t *testing.T) {
+	flags := &flagsCmd{n: true, k: 3}
+	assert.Equal(t, quickSort([]string{"4 5 9", "1 2 5", "2 2 0"}, *flags), []string{"2 2 0", "1 2 5", "4 5 9"})
+}
+
+func TestQuickSortSuffixNum(t *testing.T) {
+	flags := &flagsCmd{h: true}
+	assert.Equal(t, quickSort([]string{"1066.5B", "10.1G", "1.0K"}, *flags), []string{"1.0K", "1066.5B", "10.1G"})
+}
+
+func TestQuickSortMonth(t *testing.T) {
+	flags := &flagsCmd{M: true}
+	assert.Equal(t, quickSort([]string{"jul", "aug", "jan"}, *flags), []string{"jan", "jul", "aug"})
+}
+
+func TestQuickSortMonthReverse(t *testing.T) {
+	flags := &flagsCmd{M: true, r: true}
+	assert.Equal(t, quickSort([]string{"jul", "aug", "jan"}, *flags), []string{"aug", "jul", "jan"})
+}
+
+func TestQuickSortUnique(t *testing.T) {
+	flags := &flagsCmd{u: true}
+	assert.Equal(t, quickSort([]string{"ee", "aa", "ee"}, *flags), []string{"aa", "ee"})
+}
+func TestQuickSortUniqueReverse(t *testing.T) {
+	flags := &flagsCmd{u: true, r: true}
+	assert.Equal(t, quickSort([]string{"ee", "aa", "ee"}, *flags), []string{"ee", "aa"})
+}
+
+func TestQuickSortCheck(t *testing.T) {
+	flags := &flagsCmd{}
+	assert.True(t, checkSort([]string{"aa", "ab", "bc"}, *flags), true)
+}
